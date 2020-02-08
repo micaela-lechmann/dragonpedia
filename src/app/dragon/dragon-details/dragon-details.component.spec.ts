e70e9852f55fb619742411aa5981cbcf9c001a65
Micaela Lechmann <micaela.lechmann@gmail.com>
2020-02-08 16:58:03 -0300

    add dragon details page

--- src/app/dragon/dragon-details/dragon-details.component.spec.ts
+++ src/app/dragon/dragon-details/dragon-details.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { cold, getTestScheduler } from 'jasmine-marbles';
 import { DragonDetailsComponent } from './dragon-details.component';
+import { DragonService } from 'src/app/services/dragon.service';
+import { Dragon } from 'src/app/shared/models/dragon.model';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DragonDetailsComponent', () => {
   let component: DragonDetailsComponent;
   let fixture: ComponentFixture<DragonDetailsComponent>;
+  let service: DragonService;
+  
+  const dragonMock: Dragon = {
+    name: 'Dragon',
+    type: 'Fire',
+    createdAt: new Date(),
+    id: '1',
+    histories: []
+  }
+  const routeMock = { snapshot: {params: { id: '1' }}}
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DragonDetailsComponent ]
+      declarations: [ DragonDetailsComponent ],
+      imports: [HttpClientTestingModule],
+      providers: [ { provide: ActivatedRoute, useValue: routeMock }]
     })
     .compileComponents();
   }));
@@ -17,9 +33,22 @@ describe('DragonDetailsComponent', () => {
     fixture = TestBed.createComponent(DragonDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    service = TestBed.get(DragonService);
+    
+    const dragonObs = cold('--x|', {x: dragonMock})
+    spyOn(service, 'getDragon').and.returnValue(dragonObs);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return dragon according to id', () => {
+    component.ngOnInit();
+    getTestScheduler().flush();
+    
+    expect(service.getDragon).toHaveBeenCalledWith('1');
+    expect(component.dragon).toBe(dragonMock);
   });
 });
--- src/app/dragon/dragon-details/dragon-details.component.ts
+++ src/app/dragon/dragon-details/dragon-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { DragonService } from 'src/app/services/dragon.service';
+import { ActivatedRoute } from '@angular/router';
+import { Dragon } from 'src/app/shared/models/dragon.model';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dragon-details',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dragon-details.component.scss']
 })
 export class DragonDetailsComponent implements OnInit {
+  public dragon: Dragon;
 
-  constructor() { }
+  constructor(private dragonService: DragonService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getDragonDetails();
+  }
+
+  private getDragonDetails(): void {
+    const id = this.route.snapshot.params['id'];
+    this.dragonService.getDragon(id)
+      .pipe(take(1))
+      .subscribe(dragon => this.dragon = dragon);
   }
 
 }
