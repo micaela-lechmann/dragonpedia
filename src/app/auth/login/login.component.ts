2d77ddf1d290f85c6993fcd36d2be9a93d0e694a
Micaela Lechmann <micaela.lechmann@gmail.com>
2020-02-08 17:01:34 -0300

    add css responsive styles to login page

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { User } from 'src/app/shared/models/user.model';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -29,7 +29,7 @@ export class LoginComponent {
     })
   }
 
-  onLogin() {
+  public onLogin(): void {
     if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched();
       return;
