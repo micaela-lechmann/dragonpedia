cadf4f54abc4c20f8c9143404d7f81d17ac2bbe8
Micaela Lechmann <micaela.lechmann@gmail.com>
2020-02-08 17:00:20 -0300

    add dragon register page

--- src/app/dragon/dragon-register/dragon-register.component.spec.ts
+++ src/app/dragon/dragon-register/dragon-register.component.spec.ts
@@ -1,25 +1,69 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DragonRegisterComponent } from './dragon-register.component';
+import { DragonService } from 'src/app/services/dragon.service';
+import { Router } from '@angular/router';
+import { Dragon } from 'src/app/shared/models/dragon.model';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DragonListComponent } from '../dragon-list/dragon-list.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { cold, getTestScheduler } from 'jasmine-marbles';
 
 describe('DragonRegisterComponent', () => {
   let component: DragonRegisterComponent;
   let fixture: ComponentFixture<DragonRegisterComponent>;
+  let service: DragonService;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DragonRegisterComponent ]
+      declarations: [DragonRegisterComponent, DragonListComponent],
+      imports: [
+        SharedModule,
+        RouterTestingModule.withRoutes([
+          { path: 'dragon/list', component: DragonListComponent }
+        ]),
+        HttpClientTestingModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DragonRegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    service = TestBed.get(DragonService);
+    router = TestBed.get(Router);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onSubmit', () => {
+    it('should call create method and redirect to list', (done) => {
+      const newDragon: Dragon = {
+        name: 'Dragon',
+        type: 'Ice',
+        createdAt: new Date('2020-01-01'),
+        id: '1',
+        histories: []
+      }
+      const dragonObs = cold('--x|', {x: newDragon});
+
+      spyOn(service, 'createDragon').and.returnValue(dragonObs);
+      component.onSubmit(newDragon);
+      getTestScheduler().flush();
+
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(service.createDragon).toHaveBeenCalledWith(newDragon);
+        expect(router.url).toBe('/dragon/list');
+        done();
+      });
+    });
   });
 });
--- src/app/dragon/dragon-register/dragon-register.component.ts
+++ src/app/dragon/dragon-register/dragon-register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DragonService } from 'src/app/services/dragon.service';
+import { Dragon } from 'src/app/shared/models/dragon.model';
+import { Router } from '@angular/router';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dragon-register',
   templateUrl: './dragon-register.component.html',
   styleUrls: ['./dragon-register.component.scss']
 })
-export class DragonRegisterComponent implements OnInit {
+export class DragonRegisterComponent {
 
-  constructor() { }
+  constructor(private dragonService: DragonService,
+    private router: Router) { }
 
-  ngOnInit() {
+  public onSubmit(dragon: Dragon): void {
+    this.dragonService.createDragon(dragon)
+      .pipe(take(1)).subscribe(() => {
+      this.router.navigate(['/dragon/list']);
+    });
   }
 
 }
