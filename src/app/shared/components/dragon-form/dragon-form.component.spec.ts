024701bd7d15c9702405524dc3a9c3e14cda0bd7
Micaela Lechmann <micaela.lechmann@gmail.com>
2020-02-08 17:02:01 -0300

    add form compoment

--- src/app/shared/components/dragon-form/dragon-form.component.spec.ts
+++ src/app/shared/components/dragon-form/dragon-form.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DragonFormComponent } from './dragon-form.component';
+import { SharedModule } from '../../shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DragonBtnComponent } from '../dragon-btn/dragon-btn.component';
+import { DragonInputComponent } from '../dragon-input/dragon-input.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { format } from 'url';
+import { Dragon } from '../../models/dragon.model';
 
 describe('DragonFormComponent', () => {
   let component: DragonFormComponent;
@@ -8,7 +15,11 @@ describe('DragonFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DragonFormComponent ]
+      declarations: [ DragonFormComponent, DragonBtnComponent, DragonInputComponent ],
+      imports: [
+        ReactiveFormsModule,
+        FontAwesomeModule
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +32,71 @@ describe('DragonFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe(('dragonForm'), () => {
+    it('should be empty if dragon does not exist', () => {
+      component.ngOnInit();
+
+      expect(component.dragonForm.get('name').value).toBe('');
+      expect(component.dragonForm.get('type').value).toBe('');
+    });
+
+    it('should be filled by dragon info if it exists', () => {
+      component.dragon = {
+        histories: [],
+        name: 'Test Dragon',
+        type: 'Ice',
+        createdAt: new Date(),
+        id: '1'
+      };
+      component.ngOnInit();
+
+      expect(component.dragonForm.get('name').value).toBe('Test Dragon');
+      expect(component.dragonForm.get('type').value).toBe('Ice');
+    });
+  });
+
+  describe('onClick', () => {
+    it('should not emit submit if form is invalid', () => {
+      spyOn(component.submit, 'emit');
+      component.dragonForm.setValue({ 'name': '', 'type': ''});
+      component.onClick();
+
+      expect(component.submit.emit).not.toHaveBeenCalled();
+    });
+
+    it('should emit updated dragon values from form if dragon exists', () => {
+      const expectedDragon: Dragon = {
+        name: 'Test',
+        type: 'Fire'
+      };
+      spyOn(component.submit, 'emit');
+      component.dragonForm.setValue({ 'name': 'Test', 'type': 'Fire'});
+      component.onClick();
+
+      expect(component.submit.emit).toHaveBeenCalledWith(expectedDragon);
+    });
+
+    it('should emit updated dragon values from form if dragon exists', () => {
+      component.dragon = {
+        histories: [],
+        name: 'Test Dragon',
+        type: 'Ice',
+        createdAt: new Date(),
+        id: '1'
+      };
+      const expectedDragon: Dragon = {
+        ...component.dragon,
+        name: 'Test',
+        type: 'Fire'
+      };
+
+      spyOn(component.submit, 'emit');
+      component.dragonForm.setValue({ 'name': 'Test', 'type': 'Fire'});
+      component.onClick();
+
+      expect(component.submit.emit).toHaveBeenCalledWith(expectedDragon);
+    });
   });
 });
--- src/app/shared/components/dragon-form/dragon-form.component.ts
+++ src/app/shared/components/dragon-form/dragon-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Dragon } from '../../models/dragon.model';
 
 @Component({
   selector: 'app-dragon-form',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DragonFormComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public dragon: Dragon = {} as Dragon;
+  @Output()
+  public submit = new EventEmitter();
+  public dragonForm: FormGroup;
+
+  public nameErrorMessages = {
+    "required": "Please, enter the dragon's name."
+  };
+  public typeErrorMessages = {
+    "required": "Please, enter the dragon's type."
+  };
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.dragonForm = this.fb.group({
+      name: [this.dragon.name || '', [Validators.required]],
+      type: [this.dragon.type || '', [Validators.required]],
+    });
+  }
+
+  onClick(): void {
+    if (this.dragonForm.invalid) {
+      this.dragonForm.markAllAsTouched();
+      return;
+    }
+
+    const dragon: Dragon = {
+      ...this.dragon,
+      name: this.dragonForm.get('name').value,
+      type: this.dragonForm.get('type').value
+    }
+    
+    this.submit.emit(dragon);
   }
 
 }
